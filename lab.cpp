7456436ac03ca0f3b4ff9c97c16ad5a2e19e9b2e
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2022-09-15 16:20:07 +0300

    Update lab.cpp

--- lab.cpp
+++ lab.cpp
@@ -1,3 +1,42 @@
+#lab 1
+
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+
+
+string moveCharToLeft(string word) {
+    word.insert(0, 1, word[word.length() - 1]);
+    word.erase(word.length() - 1, 1);
+    return word;
+}
+
+string decodeWord(string word) {
+    for (int i = 0; i < 3; i++) {
+        word = moveCharToLeft(word);
+    }
+    return word;
+}
+
+void decode(string sentence) {
+    string resultSentence = "";
+    stringstream ss(sentence);
+    string word;
+    while (ss >> word) {
+        resultSentence += decodeWord(word) + " ";
+    }
+    cout << resultSentence << endl;
+}
+
+int main(int argc, const char * argv[]) {
+    string sentence = "ethingSom  ethingSom ethingSom";
+    decode(sentence);
+    return 0;
+}
+
+# lab 2
+
 #include <iostream>
 #include <fstream>
 
