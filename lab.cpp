59bd296f89196c3f865f3824f576b9afccaadd8c
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2022-09-18 12:59:48 +0300

    update lab 3

--- lab.cpp
+++ lab.cpp
@@ -77,3 +77,52 @@ int main()
 {
 	read();
 }
+
+
+
+//lab 3
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <fstream>
+
+using namespace std;
+
+
+void generateGrayarr(int n)
+{
+
+    ofstream codes1("C:\\Users\\Alexander\\Desktop\\codes1.txt");
+    if (n <= 0)
+        return;
+    vector<string> arr;
+    arr.push_back("0");
+    arr.push_back("1");
+    int i, j;
+    for (i = 2; i < (1 << n); i = i << 1)
+    {
+
+        for (j = i - 1; j >= 0; j--)
+            arr.push_back(arr[j]);
+
+        for (j = 0; j < i; j++)
+            arr[j] = "0" + arr[j];
+
+        for (j = i; j < 2 * i; j++)
+
+            arr[j] = "1" + arr[j];
+
+    }
+    for (i = 0; i < arr.size(); i++)
+        codes1 << arr[i] << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    generateGrayarr(n);
+    return 0;
+}
